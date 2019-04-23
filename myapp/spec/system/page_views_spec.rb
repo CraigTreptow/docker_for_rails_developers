38faa710d6d4704153500517d7c872b9fe2741ef
Craig Treptow <craig.treptow@gmail.com>
2019-04-22 21:29:25 -0500

    task[system test]: Sets up testing with Javascript
    We did:
    <modify app/views/welcome/index.html.erb>
    <modify app/views/layouts/application.html.erb?
    <modify spec/system/*>
    <modify Gemfile>
    docker-compose build web
    docker-compose stop web
    docker-compose up -d --force-recreate web
    <modify docker-compose.yml>
    docker-compose up -d selenium_chrome
    <create spec/support/capybara.rb>
    <modify spec/rails_helper.rb>
    docker-compose exec web rspec spec/system

--- myapp/spec/system/page_views_spec.rb
+++ myapp/spec/system/page_views_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe 'PageViews' do
 
     expect(page.text).to match(/This page has been viewed [0-9]+ times!/)
   end
+
+  it 'is enhanced with Javascript on', js: true do
+    visit '/welcome'
+
+    expect(page).to have_text('ENHANCED!')
+  end
 end
